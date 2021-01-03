bb147b8cd96637034917a36267cb3c21c77cd571
Vinci Hsieh <vinci@xiewenqideMacBook-Pro.local>
2021-01-03 16:57:20 +0800

    done

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # NBA-Salary-Prediction
+
 # NBA-Salary-Prediction
+
+'POS'：位置
+'AGE',：年紀
+'MIN%'：出賽時間比例
+'USG%'：球員使用率
+'TO%'：失誤率
+'FT%'：罰球命中率
+'2P%'：二分球命中率
+'3P%'：三分球命中率
+'eFG%'：有效投籃命中率
+'TS%'：真實命中率
+'PPG'：場均得分
+'RPG'：場均籃板
+'TRB%'：總籃板率
+'APG'：場均助攻
+'AST%'：助功率
+'SPG'：場均抄截
+'BPG'：場均阻攻
+'TOPG'：場均失誤
+'VI'：多功能指數
+'ORTG'：進攻效率
+'DRTG'：防守效率
---
+++ code.py
@@ -0,0 +1,96 @@
+import sys
+import numpy
+import pandas
+import sklearn.metrics
+import sklearn.model_selection
+import sklearn.linear_model
+import sklearn.preprocessing
+from sklearn import linear_model
+from sklearn import metrics
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.datasets import make_regression
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+
+def load_train_test_data(train_ratio=.7):
+    data = pandas.read_csv('./stats.csv')
+    feature_col = ['POS', 'AGE', 'MIN%', 'USG%', 'TO%', 'FT%', '2P%', '3P%', 'eFG%', 'TS%',
+                   'PPG', 'RPG', 'TRB%', 'APG', 'AST%', 'SPG', 'BPG', 'TOPG', 'VI', 'ORTG', 'DRTG']
+    label_col = ['SALARY']
+    X = data[feature_col]
+    y = data[label_col]
+
+    return sklearn.model_selection.train_test_split(X, y, test_size=1 - train_ratio, random_state=0)
+
+
+def load_precise_train_test_data(train_ratio=.7):
+    precise_data = pandas.read_csv('./stats.csv')
+    feature_col = ['AGE', 'PPG', 'APG', 'RPG', 'SPG']
+    label_col = ['SALARY']
+    X = precise_data[feature_col]
+    y = precise_data[label_col]
+
+    return sklearn.model_selection.train_test_split(X, y, test_size=1 - train_ratio, random_state=0)
+
+
+def train_predict(X_train, X_test, y_train, y_test, precise):
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+    y_pred = lr.predict(X_test)
+    print("train score: ", lr.score(X_train, y_train))
+    print("test score: ", lr.score(X_test, y_test))
+    print("------")
+    print('MAE:', metrics.mean_absolute_error(y_test, y_pred))
+    print('RMSE:', numpy.sqrt(
+        metrics.mean_squared_error(y_test, y_pred)))
+    print("------")
+    for i in range(len(X_train.columns)):
+        plt.scatter(X_train.iloc[:, i], y_train['SALARY'], label='train')
+        plt.scatter(X_test.iloc[:, i], y_test['SALARY'],
+                    label='test', color='green')
+        plt.scatter(X_test.iloc[:, i], y_pred, label='predict', color='red')
+        plt.title("NBA Salary Prediction")
+        plt.legend(loc='upper left')
+        plt.xlabel(str(X_train.columns[i]))
+        plt.ylabel('Ten Million Dollors')
+        if precise:
+            plt.savefig(str(X_train.columns[i])+"_precise.png")
+        else:
+            plt.savefig(str(X_train.columns[i])+".png")
+        plt.clf()
+    return lr
+
+
+def salary_prediction(lr, age, ppg, apg, rpg, spg):
+    player = [
+        [age, ppg, apg, rpg, spg]
+    ]
+    df = pandas.DataFrame(player, columns=['AGE', 'PPG', 'APG', 'RPG', 'SPG'])
+    salary = lr.predict(df)
+    print("你預測在NBA會拿到的薪水為：$", int(salary[0, 0]))
+
+
+def main(argv):
+    # using all data
+    X_train, X_test, y_train, y_test = load_train_test_data(train_ratio=.7)
+    X_train_precise, X_test_precise, y_train_precise, y_test_precise = load_precise_train_test_data(
+        train_ratio=.7)
+    # using precise data
+    lr = train_predict(X_train, X_test, y_train, y_test, precise=False)
+    lr = train_predict(X_train_precise, X_test_precise,
+                       y_train_precise, y_test_precise, precise=True)
+    # input
+    age = input('AGE: ')
+    ppg = input('PPG: ')
+    apg = input('APG: ')
+    rpg = input('RPG: ')
+    spg = input('SPG: ')
+    salary_prediction(lr, float(age), float(
+        ppg), float(apg), float(rpg), float(spg))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
