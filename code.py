2e5775fcb7bad0c33e4ffcf893201cfc1a93b0f4
Vinci Hsieh <vinci@xiewenqideMacBook-Pro.local>
2021-01-04 05:45:07 +0800

    rewrite predict

--- code.py
+++ code.py
@@ -11,9 +11,9 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 
-def load_train_test_data(train_ratio=.7):
-    data = pandas.read_csv('./stats.csv')
-    feature_col = ['POS', 'AGE', 'MIN%', 'USG%', 'TO%', 'FT%', '2P%', '3P%', 'eFG%', 'TS%',
+def load_train_test_data(train_ratio):
+    data = pandas.read_csv('./train_test.csv')
+    feature_col = ['AGE', 'MIN%', 'USG%', 'TO%', 'FT%', '2P%', '3P%', 'eFG%', 'TS%',
                    'PPG', 'RPG', 'TRB%', 'APG', 'AST%', 'SPG', 'BPG', 'TOPG', 'VI', 'ORTG', 'DRTG']
     label_col = ['SALARY']
     X = data[feature_col]
@@ -22,17 +22,20 @@ def load_train_test_data(train_ratio=.7):
     return sklearn.model_selection.train_test_split(X, y, test_size=1 - train_ratio, random_state=0)
 
 
-def load_precise_train_test_data(train_ratio=.7):
-    precise_data = pandas.read_csv('./stats.csv')
-    feature_col = ['AGE', 'PPG', 'APG', 'RPG', 'SPG']
+def load_players_data():
+    data = pandas.read_csv('./players.csv')
+    feature_col = ['AGE', 'MIN%', 'USG%', 'TO%', 'FT%', '2P%', '3P%', 'eFG%', 'TS%',
+                   'PPG', 'RPG', 'TRB%', 'APG', 'AST%', 'SPG', 'BPG', 'TOPG', 'VI', 'ORTG', 'DRTG']
+    name_col = ['NAME']
     label_col = ['SALARY']
-    X = precise_data[feature_col]
-    y = precise_data[label_col]
+    X = data[feature_col]
+    y = data[label_col]
+    name = data[name_col]
 
-    return sklearn.model_selection.train_test_split(X, y, test_size=1 - train_ratio, random_state=0)
+    return X, y, name
 
 
-def train_predict(X_train, X_test, y_train, y_test, precise):
+def train_predict(X_train, X_test, y_train, y_test):
     lr = LinearRegression()
     lr.fit(X_train, y_train)
     y_pred = lr.predict(X_test)
@@ -48,7 +51,7 @@ def train_predict(X_train, X_test, y_train, y_test, precise):
         metrics.mean_squared_error(y_test, y_pred)))
     print("------")
 
-    # picture
+    # draw picture
     for i in range(len(X_train.columns)):
         plt.scatter(X_train.iloc[:, i], y_train['SALARY'], label='train')
         plt.scatter(X_test.iloc[:, i], y_test['SALARY'],
@@ -58,42 +61,26 @@ def train_predict(X_train, X_test, y_train, y_test, precise):
         plt.legend(loc='upper left')
         plt.xlabel(str(X_train.columns[i]))
         plt.ylabel('Ten Million Dollors')
-        if precise:
-            plt.savefig(str(X_train.columns[i])+"_precise.png")
-        else:
-            plt.savefig(str(X_train.columns[i])+".png")
+        plt.savefig("./stats_pic/"+str(X_train.columns[i])+".png")
         plt.clf()
     return lr
 
 
-def salary_prediction(lr, age, ppg, apg, rpg, spg):
-    player = [
-        [age, ppg, apg, rpg, spg]
-    ]
-    df = pandas.DataFrame(player, columns=['AGE', 'PPG', 'APG', 'RPG', 'SPG'])
-    salary = lr.predict(df)
-    print("你預測在NBA會拿到的薪水為：$", int(salary[0, 0]))
+def predict_salary(lr, X, y, name):
+    salary = lr.predict(X)
+    for i in range(len(name)):
+        print(name.iloc[i, 0], "原本薪水為：$", y.iloc[i, 0],
+              "\t預測薪水為：$", int(salary[i, 0]))
 
 
 def main(argv):
-    # using all data
+    # load and split data
     X_train, X_test, y_train, y_test = load_train_test_data(train_ratio=.7)
-    X_train_precise, X_test_precise, y_train_precise, y_test_precise = load_precise_train_test_data(
-        train_ratio=.7)
+    lr = train_predict(X_train, X_test, y_train, y_test)
 
-    # using precise data
-    lr = train_predict(X_train, X_test, y_train, y_test, precise=False)
-    lr = train_predict(X_train_precise, X_test_precise,
-                       y_train_precise, y_test_precise, precise=True)
-
-    # input
-    age = input('AGE: ')
-    ppg = input('PPG: ')
-    apg = input('APG: ')
-    rpg = input('RPG: ')
-    spg = input('SPG: ')
-    salary_prediction(lr, float(age), float(
-        ppg), float(apg), float(rpg), float(spg))
+    # using data to predict salary
+    X, y, name = load_players_data()
+    predict_salary(lr, X, y, name)
 
 
 if __name__ == "__main__":
